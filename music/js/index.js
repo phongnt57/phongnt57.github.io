0a52ff22eb4501d92c1e709ac657b0f31083aeae
phong <phongnt@vnext.com.vn>
2018-12-01 10:54:24 +0700

    fix music player.

--- music/js/index.js
+++ music/js/index.js
@@ -1,3 +1,57 @@
+// Play audio
+$(document).ready(function () {
+    init();
+    function init(){
+    	var song = ["The night we met","Say you will","Lặng thầm ","You to me, me to you"];
+    	var artist = ["Lord Huron","Tokyo Square","Thuy Chi","Scenery of Riding Bicycle"];
+        var current = 0;
+        var audio = $('#audio');
+        var playlist = $('#playlist');
+        var tracks = playlist.find('li a');
+        var len = tracks.length - 1;
+        //audio[0].volume = .10;
+        audio[0].play();
+        playlist.on('click','.list_item', function(e){
+            e.preventDefault();
+            current = $(this).index();
+
+            link = $(this).find('a');
+            console.log(link.text());
+            console.log("current")
+			console.log(current)
+            $('.mini-player').find(".title").text(song[current]);
+            $('.mini-player').find(".artist").text( artist[current]);
+            $('.playback_wrapper').find(".title").text(song[current]);
+            $('.playback_wrapper').find(".artist").text(artist[current]);
+
+
+
+
+            run(link, audio[0]);
+        });
+        audio[0].addEventListener('ended',function(e){
+            current++;
+            if(current == len){
+                current = 0;
+                link = playlist.find('a')[0];
+            }else{
+                link = playlist.find('a')[current];
+            }
+            $('.mini-player').find(".title").text(song[current]);
+            $('.mini-player').find(".artist").text( artist[current]);
+            $('.playback_wrapper').find(".title").text(song[current]);
+            $('.playback_wrapper').find(".artist").text(artist[current]);
+            run($(link),audio[0]);
+        });
+    }
+    function run(link, player){
+        player.src = link.attr('href');
+        par = link.parent();
+        // par.addClass('active').siblings().removeClass('active');
+        player.load();
+        player.play();
+    }
+});
 // ===== Open Nav =====
 $( ".burger-wrapper" ).click(function() {
 	
